900a2dd55b31a28bb592b16286678e6f0601f664
tianshiyu <1055052061@qq.com>
2020-11-08 23:18:17 +0800

    备忘录邮件功能,添加两个漏下的文件

---
+++ src/main/java/org/dishi/entity/Memo.java
@@ -0,0 +1,65 @@
+package org.dishi.entity;
+
+import java.util.Date;
+
+public class Memo {
+    private Integer mid;
+
+    private Date createtime;
+
+    private Date sendtime;
+
+    private Integer uid;
+
+    private Integer state;
+
+    private String content;
+
+    public Integer getMid() {
+        return mid;
+    }
+
+    public void setMid(Integer mid) {
+        this.mid = mid;
+    }
+
+    public Date getCreatetime() {
+        return createtime;
+    }
+
+    public void setCreatetime(Date createtime) {
+        this.createtime = createtime;
+    }
+
+    public Date getSendtime() {
+        return sendtime;
+    }
+
+    public void setSendtime(Date sendtime) {
+        this.sendtime = sendtime;
+    }
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    public Integer getState() {
+        return state;
+    }
+
+    public void setState(Integer state) {
+        this.state = state;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
